88f4abf93e4e7ddd4977c17170ee9a929e111667
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-06-10 17:09:10 +0300

    addding a database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-spherical-98498',
-    user: 'postgres',
-    password: 'd-*sapni',
-    database: 'blitz-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
