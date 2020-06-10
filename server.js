31781e80d4ba349d52900fd0ae5d5233782c3bfc
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-06-10 16:45:45 +0300

    changing the response

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const knex = require('knex');
 
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'd-*sapni',
-      database : 'blitz-brain'
-    }
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'd-*sapni',
+    database: 'blitz-brain'
+  }
 });
 
 const app = express();
@@ -24,12 +24,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => { res.send(db.users) })
+app.get("/", (req, res) => { res.send("Epic! Everything is working!") })
 app.post("/signin", signin.handleSignIn(db, bcrypt))
-app.post("/register", (req, res) =>  { register.handleRegister(req, res, db, bcrypt) })
-app.get("/profile/:id", (req,res) => { profile.handleProfileGet(req,res, db)})
-app.put("/image", (req,res) => { image.handleImage(req,res, db) })
-app.post("/imageurl", (req,res) => { image.handleAPICall(req,res) })
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put("/image", (req, res) => { image.handleImage(req, res, db) })
+app.post("/imageurl", (req, res) => { image.handleAPICall(req, res) })
 // bcrypt.hash("bacon", null, null, function (err, hash) {
 //     // Store hash in your password DB.
 // });
@@ -43,7 +43,7 @@ app.post("/imageurl", (req,res) => { image.handleAPICall(req,res) })
 // });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Everything is working on port ${process.env.PORT}`)
+  console.log(`Everything is working on port ${process.env.PORT}`)
 })
 
 
